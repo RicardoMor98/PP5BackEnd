7b59eee24a67a781cefadd1aaeff7f519a3bcde5
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2025-05-27 22:35:52 +0000

    Add: Deployment settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p^2v$bbtrm(=5mde5!!gk-rpp_gs6ffgv+2d#1(x_lz0a3id52'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ricardomor98-pp5backend-xanoqb6rxk8.ws-eu119.gitpod.io',
                 '.herokuapp.com',]
