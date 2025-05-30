5eed881631c84ddc6249bd3298f1dc3583d352af
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2025-05-30 00:45:55 +0000

    Add: Create urls in main

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/', include('tryveapp.urls')),
+    path('api/auth/', include('users.urls')),  # if you added auth views
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
