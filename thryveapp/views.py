018cd7def7af15d0c6103bc5f85e8b793e5fe746
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2025-05-30 00:39:25 +0000

    Add: Create views

--- thryveapp/views.py
+++ thryveapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from .models import UserProfile, Post, Category, Comment, Vote
+from .serializers import (
+    UserProfileSerializer, PostSerializer, CategorySerializer,
+    CommentSerializer, VoteSerializer
+)
+from .permissions import IsOwnerOrReadOnly
 # Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
