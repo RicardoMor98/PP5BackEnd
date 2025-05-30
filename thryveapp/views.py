05e771ba80baf17f2550306c08944365b532a88d
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2025-05-30 00:40:47 +0000

    Add: Comment views

--- thryveapp/views.py
+++ thryveapp/views.py
@@ -20,3 +20,8 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
